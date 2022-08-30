9cccab816e980b286e8bb5daf35bebb7b22614ae
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2022-08-30 13:42:54 -0600

    Create mild830.py

---
+++ mild830.py
@@ -0,0 +1,8 @@
+f = input("what is your age? ")
+f = int(f)
+if f <= 13:
+    print("you are too young to play this game. come back when your older.")
+elif f <= 17:
+    print("your gonna need you're parents permission to play this you baby")
+elif f > 17:
+    print("welcome to the game Hero!")

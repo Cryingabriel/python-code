5dd8fe74603736757f59b580e9df9cae175feb8b
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2022-09-02 12:49:39 -0600

    y

---
+++ standard.py
@@ -0,0 +1,9 @@
+l = input("what is you're most recent score?")
+f = int(l)
+
+if f < 100:
+    print("you stink boi/girl/person/it")
+elif f >= 100 and f <= 800:
+    print("eh I guess your pretty good")
+if f > 800:
+    print("dang you're amazing")
---
+++ standerd2.py
@@ -0,0 +1,6 @@
+
+
+for i in range(8, 28, 4):
+    print(i)
+for i in range(80, 72):
+    print(i)

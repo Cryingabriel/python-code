119c222af0b03a936d8682e7b647ee0a702eeba1
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2022-09-06 13:16:14 -0600

    Create 9.6D.py

---
+++ 9.6D.py
@@ -0,0 +1,11 @@
+print("loop #1:")
+for i in range(20):
+    print(i, end = "")
+    
+print("\n\nloop #2:") # \n means skip a line
+for j in range(-4, 9):
+    print(j, end ="")
+    
+print("\n\loop #3:")
+for k in range(50, 20, -5):
+    print(k, end ="")

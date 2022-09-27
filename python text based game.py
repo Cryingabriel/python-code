115a96b90f974627e5a8af715df1039674b4ab03
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2022-09-27 13:38:06 -0600

    Update python text based game.py

--- python text based game.py
+++ python text based game.py
@@ -1,6 +1,6 @@
 
 
-print("you slowly wake up with water driping down you're face, when you open your eyes your inside a cold dark and damp room. You have no idea how you woke up in here, all you can recall is falling asleep in bed then waking here")
+print("you slowly wake up with water driping down you're face, when you open your eyes your inside a cold, dark and damp room. You have no idea how you woke up in here, all you can recall is falling asleep in bed then waking here.")
 room = 1
 while True:
     if room == 1:
@@ -22,8 +22,15 @@ while True:
             room = 2
         elif choice == 's' or choice == 'S' or choice == 'south' or choice == 'South':
             room = 4
+        elif choice == 'e' or choice == 'E' or choice == 'east' or choice == 'East':
+            room = 5
     if room == 4:
         print("your in room 4, you can move north")
         choice = input()
         if choice == 'n' or choice == 'N' or choice == 'north' or choice == 'North':
+            room = 3
+     if room == 5:
+        print("your in room 5, you can move north")
+        choice = input()
+        if choice == 'w' or choice == 'W' or choice == 'west' or choice == 'West':
             room = 3

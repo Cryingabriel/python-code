bc6a98fff991b589122cb7c31fe3f6172be9f42e
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2022-10-05 14:44:32 -0600

    Update python text based game.py

--- python text based game.py
+++ python text based game.py
@@ -4,40 +4,109 @@ def monster(biome):
   num = random.randrange(0, 100)
   if biome == "dungeon":
     if num <= 25:
-      print("a zombie attacks you")
+      battlesystem("Zombie",100)
     elif num <= 45:#30% chance
-      print("a skeleton fires at you")
+      battlesystem("Skeleton",100)
     elif num <= 65:
-      print("a spider is trying to trap you")
+      battlesystem("Spider",100)
     elif num <= 100:
-      print("you get stabbed by a goblin")
+      battlesystem("Goblin",100)
   elif biome == "mansion":
     if num <= 30:
-      print("a scorpion attacks you")
+      battlesystem("Scorpion",100)
     elif num <= 50:#30% chance
-      print("a skeleton fires at you")
+      battlesystem("Skeleton",100)
     elif num <= 79:
-      print("a spider is trying to trap you")
+      battlesystem("Spider",100)
     elif num <= 100:
-      print("you get stabbed by a goblin")
+      battlesystem("Goblin",100)
   elif biome == "forest":
     if num <= 25:
-      print("a mushroom monsters attacks you")
+      battlesystem("Mushroom",100)
     elif num <= 75:#30% chance
-      print("Fairies start biting at you")
+      battlesystem("Fairy",100)
     elif num <= 85:
-      print("a Troll is trying to squash you")
+      battlesystem("Troll",100)
     elif num <= 100:
-      print("you get stabbed by a goblin")
+      battlesystem("Goblin",100)
   elif biome == "outside":
     if num <= 25:
-      print("a Skeletons have appeared")
+      battlesystem("Skeleton",100)
     elif num <= 75:#30% chance
-      print("Ghosts are trying take you away")
+      battlesystem("Ghost",100)
     elif num <= 85:
-      print("a hored of zombies are aproaching you")
+      battlesystem("Zombie",100)
     elif num <= 100:
-      print("slimes are trying to erode you")
+      battlesystem("Slime",100)
+#____________________________________________
+
+def battlesystem(monster, playerhealth):
+    if monster == "Zombie":
+        monsterhealth = 28
+        print("a Zombie has spawned in")
+    elif monster == "Skeleton":
+        monsterhealth = 25
+        print("a Skeleton has shot at you")
+    elif monster == "Spider":
+        monsterhealth = 27
+        print("a Spider tried to trap you")
+    elif monster == "Goblin":
+        monsterhealth = 20
+        print("a Goblin is trying to stab you")
+    elif monster == "Scorpion":
+        monsterhealth = 24
+        print("A Scorpion is stinging you")
+    elif monster == "Mushroom":
+        monsterhealth =16
+        print("a mushroom monster tries to poison you")
+    elif monster == "Troll":
+        monsterhealth =45
+        print("a Troll is trying to squash you")
+    elif monster == "Fairy":
+        monsterhealth =19
+        print("Fairies start biting at you")
+    elif monster == "Ghost":
+        monsterhealth =39
+        print("Ghosts are trying take you away")
+    elif monster == "Slime":
+        monsterhealth =30
+        print("slimes are trying to erode you")
+        
+    while monsterhealth>0 and playerhealth >0:
+        if monster == "Zombie":
+            monsterattack = random.randrange(9, 14)
+        elif monster == "Skeleton":
+            monsterattack = random.randrange(8,16)
+        elif monster == "Spider":
+            monsterattack = random.randrange(7,13)
+        elif monster == "Goblin":
+            monsterattack = random.randrange(9,17)
+        elif monster == "Scorpion":
+            monsterattack = random.randrange(7,19)
+        elif monster == "Troll":
+            monsterattack = random.randrange(16,26)
+        elif monster == "Fairy":
+            monsterattack = random.randrange(10,15)
+        elif monster == "Mushroom":
+            monsterattack = random.randrange(13,15)
+        elif monster == "Ghost":
+            monsterattack = random.randrange(17,18)
+        elif monster == "Slime":
+            monsterattack = random.randrange(19,24)
+            
+        print("The", monster, "attacks deal", monsterattack, "damage")
+        playerhealth = playerhealth - monsterattack
+        print("your HP is at", playerhealth)
+        
+        playerattack = random.randrange(10,15)
+        print("you deal", playerattack, "damage")
+        monsterhealth= monsterhealth - playerattack
+        print("the monsters HP is at", monsterhealth)
+    if monsterhealth<= 0:
+        print("you have won")
+    else:
+        print("you have died")
+    return playerhealth
 
 print("you slowly wake up with water driping down you're face, when you open your eyes your inside a cold, dark and damp room. You have no idea how you woke up in here, all you can recall is falling asleep in bed then waking here.")
 room = 1
@@ -240,7 +309,7 @@ while True:
         print("your in room 21, you can move north, east or south")
         choice = input()
         if choice == 'e' or choice == 'E' or choice == 'east' or choice == 'East':
-            room = 
+            room = 22
         if choice == 'n' or choice == 'N' or choice == 'north' or choice == 'North':
             room = 19
         if choice == 's' or choice == 'S' or choice == 'south' or choice == 'South':

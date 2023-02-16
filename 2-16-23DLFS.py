57ff9b4be5a0c05827487384b5b49169e5407e47
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-02-16 13:44:15 -0700

    Update 2-16-23DLFS.py

--- 2-16-23DLFS.py
+++ 2-16-23DLFS.py
@@ -5,17 +5,13 @@ minutes = int(input("what is the minute you get up?"))
 def yay(hours, minutes):
     minutes -= 40
 
-    if minutes >59:
-        hours +=1
-        return hours
     if minutes < 0:
         hours -= 1
         minutes = minutes + 60
         return hours
-    if hours >23:
-        hours = 0
     if hours <0:
         hours = 23
+        return minutes
     print(hours,":", minutes)
 
 

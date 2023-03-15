300876e69359a783e4fe1b1f84bd84a02d13ba7f
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-03-15 13:59:27 -0600

    Update 3-15-23 Lists.py

--- 3-15-23 Lists.py
+++ 3-15-23 Lists.py
@@ -13,8 +13,8 @@ print("The biggest number is:", max(mah))
 print("The smallest number is :", min(mah))
 print(mah)
 for i in range(10):
-    if mah[i]+1 == mah[i+1]:
-        if mah[i+1]+1 == mah[+2]:
+    if mah[i]+1 == mah[i+1]:#this says that if one number is 2 then add one and check if the next one is actually 3
+        if mah[i+1]+1 == mah[+2]:#this checks if the box next to 3 is actually 4
             print("The list contains", mah[i], mah[i+1], mah[i+2], " so True")
     else:
         print("False")

bd9bf6a7e7f0357b147c65bc50490c35b58c9df8
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-10-09 14:23:55 -0600

    gone

--- addiply.py
+++
@@ -1,8 +0,0 @@
-import sys
-cases = int(sys.stdin.readline().rstrip())
-
-for i in range(cases):
-    line = sys.stdin.readline().rstrip()
-
-    line = line.split(" ")
-    print(int(line[0])+int(line[1]), int(line[0])*int(line[1]))

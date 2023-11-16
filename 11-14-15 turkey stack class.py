d0b58c9c6cb8febb0f4480ef23b0d4740533989f
Cryingabriel <Cai19mac@gmail.com>
2023-11-16 15:36:02 -0700

    l

--- 11-14-15 turkey stack class.py
+++ 11-14-15 turkey stack class.py
@@ -1,81 +1,139 @@
 import pygame
 from random import randrange as rr
 from pygame.math import Vector2
+import math
+
+pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 exit = False
 clock = pygame.time.Clock()
+
+xpos = 0
+ypos = 0
+score = 0
+mousepos = (xpos,ypos)
+
 
 class Turkey():
     def __init__(self, xpos, ypos):
         self.pos = Vector2(xpos,ypos)
         self.vy = 5
         self.vy2 = 10
+        self.xv = rr(10,20)
         self.num = rr(0,2)
+        self.alive = True
+        self.radius1 = 30
+        self.radius2 = 60
+        self.clicked1 = False
+        self.score = 0
     
     def draw(self):
-        if self.num == 0:
-            #Feathers
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+5, self.pos.y), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+15, self.pos.y-22), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+35, self.pos.y-35), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+55, self.pos.y-22), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+9, self.pos.y-11), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+25, self.pos.y-30), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-30), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-30), 10)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+60, self.pos.y-11), 10)
+        if self.alive == True:
+            if self.num == 0:
+                #Feathers
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+5, self.pos.y), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+15, self.pos.y-22), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+35, self.pos.y-35), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+55, self.pos.y-22), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+9, self.pos.y-11), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+25, self.pos.y-30), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-30), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-30), 10)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+60, self.pos.y-11), 10)
 
-            #Legs
-            pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+20, self.pos.y+10, 10, 30))
-            pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+40, self.pos.y+10, 10, 30))
+                #Legs
+                pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+20, self.pos.y+10, 10, 30))
+                pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+40, self.pos.y+10, 10, 30))
 
-            #Body
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+35, self.pos.y), 30)
-            pygame.draw.ellipse(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+23, self.pos.y-40, 25, 40))
-            #Face
-            pygame.draw.polygon(screen, (rr(0,255),rr(0,255),rr(0,255)), ((self.pos.x+30, self.pos.y-30), (self.pos.x+35, self.pos.y-15), (self.pos.x+41, self.pos.y-30)))
-            pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+28, self.pos.y-30), 5)
-            pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+28, self.pos.y-30), 3)
-            pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+43, self.pos.y-30), 5)
-            pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+43, self.pos.y-30), 3)
-        else:
-            #Feathers
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+5, self.pos.y), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+30, self.pos.y-32), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y-55), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+100, self.pos.y-32), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+120, self.pos.y), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+15, self.pos.y-15), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-45), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+90, self.pos.y-45), 20)
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+115, self.pos.y-12), 20)
+                #Body
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+35, self.pos.y), 30)
+                pygame.draw.ellipse(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+23, self.pos.y-40, 25, 40))
+                #Face
+                pygame.draw.polygon(screen, (rr(0,255),rr(0,255),rr(0,255)), ((self.pos.x+30, self.pos.y-30), (self.pos.x+35, self.pos.y-15), (self.pos.x+41, self.pos.y-30)))
+                pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+28, self.pos.y-30), 5)
+                pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+28, self.pos.y-30), 3)
+                pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+43, self.pos.y-30), 5)
+                pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+43, self.pos.y-30), 3)
+            elif self.num == 1:
+                #Feathers
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+5, self.pos.y), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+30, self.pos.y-32), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y-55), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+100, self.pos.y-32), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+120, self.pos.y), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+15, self.pos.y-15), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-45), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+90, self.pos.y-45), 20)
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+115, self.pos.y-12), 20)
 
-            #Legs
-            pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+40, self.pos.y+10, 20, 60))
-            pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+70, self.pos.y+10, 20, 60))
+                #Legs
+                pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+40, self.pos.y+10, 20, 60))
+                pygame.draw.rect(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+70, self.pos.y+10, 20, 60))
 
-            #Body
-            pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y), 50)
-            pygame.draw.ellipse(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-65, 40, 60))
-            #Face
-            pygame.draw.polygon(screen, (rr(0,255),rr(0,255),rr(0,255)), ((self.pos.x+55, self.pos.y-45), (self.pos.x+65, self.pos.y-15), (self.pos.x+75, self.pos.y-45)))
-            pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+50, self.pos.y-45), 10)
-            pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+50, self.pos.y-45), 5)
-            pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+80, self.pos.y-45), 10)
-            pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+80, self.pos.y-45), 5)
+                #Body
+                pygame.draw.circle(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+65, self.pos.y), 50)
+                pygame.draw.ellipse(screen, (rr(0,255),rr(0,255),rr(0,255)), (self.pos.x+45, self.pos.y-65, 40, 60))
+                #Face
+                pygame.draw.polygon(screen, (rr(0,255),rr(0,255),rr(0,255)), ((self.pos.x+55, self.pos.y-45), (self.pos.x+65, self.pos.y-15), (self.pos.x+75, self.pos.y-45)))
+                pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+50, self.pos.y-45), 10)
+                pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+50, self.pos.y-45), 5)
+                pygame.draw.circle(screen, (255, 255, 255), (self.pos.x+80, self.pos.y-45), 10)
+                pygame.draw.circle(screen, (0, 0, 0), (self.pos.x+80, self.pos.y-45), 5)
+    def addscore(self):
+        if self.alive == False:
+            self.score += 1
+        return self.score
+    def S(self):
+        self.score = 0
 
     def move(self):
+        if self.alive == True:
+            if self.num == 0:
+                self.pos.y += self.vy
+            else:
+                self.pos.y += self.vy2
+            if self.pos.y >= 800:
+                self.pos.y = -100
+            self.pos.x += self.xv
+
+    def collision(self):
         if self.num == 0:
-            self.pos.y += self.vy
-        else:
-            self.pos.y += self.vy2
-        if self.pos.y >= 800:
-            self.pos.y = -100
+            if math.sqrt((mousepos[0]-self.pos.x-35)**2 + (mousepos[1]-self.pos.y)**2)<self.radius1:
+                print("ah")
+                self.clicked1 = True
+                if self.clicked1 == True:
+                    self.alive = False
+                else:
+                    self.clicked1 = False
+        elif self.num == 1:
+            if math.sqrt((mousepos[0]-self.pos.x-60)**2 + (mousepos[1]-self.pos.y)**2)<self.radius2:
+                print("ah")
+                self.clicked1 = True
+                if self.clicked1 == True:
+                    self.alive = False
+                else:
+                    self.clicked1 = False
+
+        if self.alive == True:
+            if self.pos.x >=800:
+                self.xv *= -1
+            elif self.pos.x <=0:
+                self.xv *= -1
+        
+        if self.alive == False:
+            self.pos.x = rr(0,800)
+            self.pos.y = rr(0,800)
+            self.num = rr(0,2)
+            self.alive = True
+
+        return self.alive
+            
+        
 
 l = []
-for i in range(500):
+for i in range(100):
     l.append(Turkey(rr(0,800), rr(0,800)))
 
 
@@ -86,10 +144,27 @@ while exit == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gover = True
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(l)):
+                l[i].collision()
+
+
+        if event.type == pygame.MOUSEMOTION: #check if mouse moved
+            mousepos = event.pos #refreshes mouse position
     
     screen.fill((0,0,0))
     for i in range(len(l)):
         l[i].move()
         l[i].draw()
+
+        score += l[i].addscore()
+    for i in range(len(l)):
+        l[i].S()
+    my_font = pygame.font.SysFont('Comic Sans MS', 30)
+    text_label3 = my_font.render(str("Score: "),1,(255,0,0))
+    text_surface = my_font.render(str(score), 1 ,(255, 0, 0))
+    screen.blit(text_surface, (90,0))
+    screen.blit(text_label3, (0,0))
 
     pygame.display.flip()
